a37a2b6ac7f80148f43e02b75f9b787fcf504408
黃敏雄 <64833384+MinXiong-Huang@users.noreply.github.com>
2021-01-31 12:30:27 +0800

    Update LeetCode_1._Two_Sum.py

--- LeetCode_1._Two_Sum.py
+++ LeetCode_1._Two_Sum.py
@@ -8,10 +8,10 @@ class Solution(object):
         """
         for i in range(len(nums)-1):
             for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
+                if nums[i]+nums[j] == target:
                     return i,j
                     
 #Example
-s=Solution()
+s = Solution()
 s.twoSum([2, 7, 11, 15],9)
 
